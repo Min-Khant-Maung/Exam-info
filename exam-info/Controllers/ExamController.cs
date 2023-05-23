ebd72c1474a2cfb56fc62471ca0a9999493337a6
[Min-Khant-Maung] <[minkhant13003@gmail.com]>
2023-05-23 22:18:30 +0630

    update

--- exam-info/Controllers/ExamController.cs
+++ exam-info/Controllers/ExamController.cs
@@ -2,6 +2,7 @@
 using exam_info.Models.DAO;
 using exam_info.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -74,6 +75,70 @@ namespace exam_info.Controllers
             else
             {
                 ViewBag.msg = "Failed";
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(string id)
+        {
+            Exam exam = _applicationDBContext.Exams.Find(id);
+            if (exam != null)
+            {
+                _applicationDBContext.Exams.Remove(exam);//remove the student record from DBset
+                _applicationDBContext.SaveChanges();//remove effect to the database
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(string id)
+        {
+            ExamViewModel examViewModel = _applicationDBContext.Exams
+                .Where(w => w.Id == id)
+                .Select(s => new ExamViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Subject = s.Subject,
+                    Type = s.Type,
+                    Duration = s.Duration,
+                    Marks = s.Marks
+
+                }).SingleOrDefault();
+            return View(examViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ExamViewModel examViewModel)
+        {
+            bool isSuccess = false;
+            try
+            {
+                Exam exam = new Exam();
+                //audit columns
+                exam.Id = examViewModel.Id;
+
+                //ui columns
+              
+                exam.Name = examViewModel.Name;
+                exam.Type = examViewModel.Type;
+                exam.Subject = examViewModel.Subject;
+                exam.Marks = examViewModel.Marks;
+                exam.Duration = examViewModel.Duration;
+                _applicationDBContext.Entry(exam).State = EntityState.Modified;//Updating the existing records in DBSet
+                _applicationDBContext.SaveChanges();//Updating the records to the database
+                isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            if (isSuccess)
+            {
+                TempData["msg"] = "Update Success";
+            }
+            else
+            {
+                TempData["msg"] = "Error occur while updating";
             }
             return RedirectToAction("List");
         }
